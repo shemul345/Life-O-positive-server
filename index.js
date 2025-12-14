222b89d99cfa060286c92fb8eb14947d0c10b475
shemul345 <shemulchowdhury7684@gmail.com>
2025-12-14 14:01:20 +0600

    implement request deleted done with client side and use tanstack query

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 // Middleware
@@ -35,7 +35,7 @@ async function run() {
         query.requesterEmail = email;
       }
 
-      const cursor = donationRequestsCollection.find(query);
+      const cursor = donationRequestsCollection.find(query).sort({createdAt: -1});
       const result = await cursor.toArray();
 
       res.send(result)
@@ -49,6 +49,14 @@ async function run() {
       const result = await donationRequestsCollection.insertOne(donationRequest);
       
       res.send(result)
+    })
+
+    app.delete('/donation-requests/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      
+      const result = await donationRequestsCollection.deleteOne(query);
+      res.send(result);
     })
 
 
