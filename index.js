1fdb3d6f9e714bcccf59ab51e85572b61e33151c
shemul345 <shemulchowdhury7684@gmail.com>
2025-12-14 18:32:42 +0600

    implement firebase admin SDK with JWT verification by accesstoken

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./life-o-positive-firebase-adminsdk.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
