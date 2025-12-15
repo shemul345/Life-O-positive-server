43826a7d2f1bb8690a655e59d974ea7e3295a19f
shemul345 <shemulchowdhury7684@gmail.com>
2025-12-15 12:38:46 +0600

    user role toggling done by server and role setup in database with client site

--- index.js
+++ index.js
@@ -79,6 +79,19 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const roleInfo = req.body;
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role:roleInfo.role
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
     // Donation Request related APIs
     app.get('/donation-requests',verifyFBToken, async (req, res) => {
       const query = {}
